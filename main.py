21c349b6b822952ca9218d4c613a45279a5968fd
Loren Kuich <loren.jk3@gmail.com>
2019-07-17 11:34:27 -0700

    Added click instead of argparse

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import argparse
-parser = argparse.ArgumentParser()
-parser.parse_args()
+import click
+
+@click.command()
+@click.argument("train")
+@click.option("--imageDir", type=str, prompt="Directory of sample images", help="Directory of sample images")
+@click.option("--steps", type=int, prompt="Number of steps to train for", help="Number of steps to train for")
+def train(imageDir, steps):
+    print("Training " + imageDir + " for " + str(steps) + " steps")
+
+@click.group()
+def main():
+    pass
 
 if __name__ == "__main__":
-    pass
+    main()
