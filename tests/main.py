2aa43ccbea5c076ff6f9ecd046d10fb1e4ab6b32
Loren Kuich <loren.jk3@gmail.com>
2019-09-03 19:22:29 -0700

    Added tests and GitHub actions

---
+++ tests/main.py
@@ -0,0 +1,12 @@
+import unittest
+import env
+
+class TestClient(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_login(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
